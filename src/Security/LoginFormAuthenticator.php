8a8ef22f6f3c9828677e5ec237cedc67586f62ed
Melvin Koopmans <melvin.koopmans@gmail.com>
2019-12-07 21:16:29 +0100

    get last username from session after error

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
@@ -36,13 +37,17 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
     public function getCredentials(Request $request): array
     {
-        return [
+        $credentials = [
             'email' => $request->request->get('email'),
             'password' => $request->request->get('password'),
         ];
+
+        $request->getSession()->set(Security::LAST_USERNAME, $credentials['email']);
+
+        return $credentials;
     }
 
-    public function getUser($credentials, UserProviderInterface $userProvider): User
+    public function getUser($credentials, UserProviderInterface $userProvider): ?User
     {
         return $this->userRepository->findOneBy(['email' => $credentials['email']]);
     }
@@ -52,24 +57,9 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         return true;
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
-    {
-        // todo
-    }
-
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey): RedirectResponse
     {
         return new RedirectResponse($this->router->generate('app_homepage'));
-    }
-
-    public function start(Request $request, AuthenticationException $authException = null)
-    {
-        // todo
-    }
-
-    public function supportsRememberMe()
-    {
-        // todo
     }
 
     /**
