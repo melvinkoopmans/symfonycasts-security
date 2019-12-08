eaee7a8c2fcd8fa4875d7d01f2fa845b53292b99
Melvin Koopmans <melvin.koopmans@gmail.com>
2019-12-08 13:21:23 +0100

    display user data on account page

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,20 +2,29 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @IsGranted("ROLE_USER")
  */
-class AccountController extends AbstractController
+class AccountController extends BaseController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * @Route("/account", name="account")
      */
     public function index()
     {
+        $this->logger->debug(sprintf('Checking account page for %s', $this->getUser()->getEmail()));
+
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
         ]);
